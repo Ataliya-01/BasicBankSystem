146a285602430391437ecc01f512f5b7ffe89806
Ataliya-01 <89702899+Ataliya-01@users.noreply.github.com>
2021-10-15 15:06:00 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,40 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sparks Bank</title>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+</head>
+
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dfe4ea;">
+  <div class="container-fluid">
+      <a class="navbar-brand logo" href="index.php"><img src="img/logo.png" alt="logo" height=50px width=50px></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+    
+        <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+         <a class="mr-5 hover:text-gray-900"href="index.php">Home</a>
+         <a class="mr-5 hover:text-gray-900"href="">About</a>
+         <a class="mr-5 hover:text-gray-900"href="contactme.php">Contact</a>
+        </nav>
+    </div>
+  </div>
+</nav>
+</body>
